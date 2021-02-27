bb5e0f243ff42faf4cde58d4b0859477368a2bc0
Falsher <ivan.zzaharov@gmail.com>
2021-02-27 21:03:44 +0200

    git added mail

--- application.php
+++ application.php
@@ -3,15 +3,15 @@
 <head>
 <meta charset="UTF-8">
 <title>Создание формы обратной связи</title>
-<meta http-equiv="Refresh" content="1; URL=http://test111.april.kh.ua/"> 
+<meta http-equiv="Refresh" content="1; URL=https://github.com/Falsher/sova"> 
 </head>
 <body>
 
 <?php 
 
-$sendto   = "andrejpavlov715@gmail.com"; // почта, на которую будет приходить письмо
+$sendto   = "ivan.zzaharov@gmail.com"; // почта, на которую будет приходить письмо
 $usernamee = $_POST['name'];   // сохраняем в переменную данные полученные из поля c именем
-$username = $_POST['email'];   // сохраняем в переменную данные полученные из поля c именем
+// $username = $_POST['email'];   // сохраняем в переменную данные полученные из поля c именем
 $usertel = $_POST['phone']; // сохраняем в переменную данные полученные из поля c телефонным номером
 
 
@@ -27,9 +27,9 @@ $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
 $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
 $msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Cообщение с сайта SPA-ANDREPAV</h2>\r\n";
 $msg .= "<p><strong>Имя клиента:</strong> ".$usernamee."</p>\r\n";
-$msg .= "<p><strong>Почта клиента:</strong> ".$username."</p>\r\n";
+// $msg .= "<p><strong>Почта клиента:</strong> ".$username."</p>\r\n";
 
-$msg .= "<p><strong>Телефон заказчика:</strong> ".$usertel."</p>\r\n";
+$msg .= "<p><strong>Телефон клиента:</strong> ".$usertel."</p>\r\n";
 
 $msg .= "</body></html>";
 
