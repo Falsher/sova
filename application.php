0ec7ecd1d70c7b10b8e62bdd9326ac6e6716fc41
Falsher <ivan.zzaharov@gmail.com>
2021-02-27 21:23:51 +0200

    git added fio

--- application.php
+++ application.php
@@ -1,42 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Создание формы обратной связи</title>
-<meta http-equiv="Refresh" content="1; URL=https://github.com/Falsher/sova"> 
-</head>
-<body>
-
-<?php 
-
-$sendto   = "ivan.zzaharov@gmail.com"; // почта, на которую будет приходить письмо
-$usernamee = $_POST['name'];   // сохраняем в переменную данные полученные из поля c именем
-// $username = $_POST['email'];   // сохраняем в переменную данные полученные из поля c именем
-$usertel = $_POST['phone']; // сохраняем в переменную данные полученные из поля c телефонным номером
-
-
-
-// Формирование заголовка письма
-$subject  = "Новое сообщение";
-$headers  = "From: " . strip_tags($usermail) . "\r\n";
-$headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
-
-// Формирование тела письма
-$msg  = "<html><body style='font-family:Arial,sans-serif;'>";
-$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Cообщение с сайта SPA-ANDREPAV</h2>\r\n";
-$msg .= "<p><strong>Имя клиента:</strong> ".$usernamee."</p>\r\n";
-// $msg .= "<p><strong>Почта клиента:</strong> ".$username."</p>\r\n";
-
-$msg .= "<p><strong>Телефон клиента:</strong> ".$usertel."</p>\r\n";
-
-$msg .= "</body></html>";
-
-// отправка сообщения
-if(@mail($sendto, $subject, $msg, $headers))
-
-?>
-
-</body>
-</html>
+<?php
+$fio = $_POST['fio'];
+$phone = $_POST['phone'];
+$fio = htmlspecialchars($fio);
+$phone = htmlspecialchars($phone);
+$fio = urldecode($fio);
+$phone = urldecode($phone);
+$fio = trim($fio);
+$phone = trim($phone);
+//echo $fio;
+//echo "<br>";
+//echo $phone;
+if (mail("ivan.zzaharov@gmail.com", "Заявка с сайта", "ФИО:".$fio.". Телефон: ".$phone ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
